638772c6b2e58f46f7f28956c9cbab6efe993d9f
duranhumes <duranhumes@gmail.com>
2019-08-25 08:26:11 -0400

    updates

--- admin-api/src/transactions/repositories/Transaction.js
+++ admin-api/src/transactions/repositories/Transaction.js
@@ -13,11 +13,11 @@ export class TransactionRepository {
 
     findVendorTransactionsQuery(vendorId) {
         return new Promise(async (resolve, reject) => {
+            const query = `
+                SELECT t.id, t.created_at, v.name AS vendor_name, s.name AS student_name from ${this.tableName} t LEFT JOIN vendors v ON t.vendor_id = v.id LEFT JOIN students s ON t.student_id = s.id WHERE t.vendor_id = ?
+            `;
             const [transactions, transactionsErr] = await promiseWrapper(
-                getDBConnection().raw(
-                    `SELECT \`t\`.\`id\`, \`t\`.\`created_at\`, \`v\`.\`name\` AS vendor_name, \`s\`.\`name\` AS student_name FROM \`${this.tableName}\` AS \`t\` LEFT JOIN \`vendors\` AS \`v\` ON \`v\`.\`id\` = \`t\`.\`vendor_id\` LEFT JOIN \`students\` AS \`s\` ON \`s\`.\`id\` = \`t\`.\`student_id\` WHERE \`t\`.\`vendor_id\` = ?`,
-                    vendorId
-                )
+                getDBConnection().raw(query, vendorId)
             );
 
             if (transactionsErr) {
@@ -35,11 +35,11 @@ export class TransactionRepository {
 
     findStudentTransactionsQuery(studentId) {
         return new Promise(async (resolve, reject) => {
+            const query = `
+                SELECT t.id, t.created_at, v.name AS vendor_name, s.name AS student_name from ${this.tableName} t LEFT JOIN vendors v ON t.vendor_id = v.id LEFT JOIN students s ON t.student_id = s.id WHERE t.student_id = ?
+            `;
             const [transactions, transactionsErr] = await promiseWrapper(
-                getDBConnection().raw(
-                    `SELECT \`t\`.\`id\`, \`t\`.\`created_at\`, \`v\`.\`name\` AS vendor_name, \`s\`.\`name\` AS student_name FROM \`${this.tableName}\` AS \`t\` LEFT JOIN \`vendors\` AS \`v\` ON \`v\`.\`id\` = \`t\`.\`vendor_id\` LEFT JOIN \`students\` AS \`s\` ON \`s\`.\`id\` = \`t\`.\`student_id\` WHERE \`t\`.\`student_id\` = ?`,
-                    studentId
-                )
+                getDBConnection().raw(query, studentId)
             );
 
             if (transactionsErr) {
